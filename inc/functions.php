35b6a2483ef4347f3b1d486b553d5d24bae779ef
imath <imathi.eu@outlook.fr>
2022-07-20 13:47:30 +0200

    Use a better way to handle wp-emojis

--- inc/functions.php
+++ inc/functions.php
@@ -88,95 +88,27 @@ function activity_do_blocks( $content ) {
 add_filter( 'bp_get_activity_content_body', __NAMESPACE__ . '\activity_do_blocks', 9 );
 
 /**
- * Make sure the WP Emoji output is containing all needed attributes.
+ * Make sure only Emoji chars are saved into the DB.
  *
  * @since 1.0.0
  *
  * @param string $activity_content The activity content.
  * @return string The sanitized activity content.
  */
-function activity_blocks_kses( $activity_content ) {
-	$emojis = array();
+function activity_blocks_preserve_emoji_chars( $activity_content ) {
+	preg_match_all( '/\<img[^>]*alt=\"([^"]*)\".?\>/', $activity_content, $matches );
 
-	preg_match_all( '%(<!--.*?(-->|$))|(<[^>]*(>|$)|>)%', $activity_content, $tags );
-	$tags = reset( $tags );
-
-	if ( $tags ) {
-		foreach ( $tags as $tag ) {
-			preg_match( '%^<\s*(/\s*)?([a-zA-Z0-9-]+)([^>]*)>?$%', wp_kses_stripslashes( $tag ), $matches );
-
-			if ( isset( $matches[2] ) && 'img' === $matches[2] ) {
-				$attributes = wp_kses_hair( $tag, wp_allowed_protocols() );
-
-				if ( isset( $attributes['class']['value'] ) && in_array( $attributes['class']['value'], array( 'wp-smiley', 'emoji' ), true ) ) {
-					if ( 'wp-smiley' === $attributes['class']['value'] ) {
-						$bp_should_allow = wp_kses(
-							$tag,
-							array(
-								'img' => array(
-									'src'   => true,
-									'alt'   => true,
-									'class' => true,
-									'style' => true,
-								),
-							)
-						);
-					} else {
-						$bp_should_allow = wp_kses(
-							$tag,
-							array(
-								'img' => array(
-									'draggable' => true,
-									'role'      => true,
-									'class'     => true,
-									'alt'       => true,
-									'src'       => true,
-								),
-							)
-						);
-					}
-
-					$emojis[] = array(
-						'bp_should_allow' => $bp_should_allow,
-						'bp_sanitized'    => bp_activity_filter_kses( $tag ),
-					);
-				}
+	if ( isset( $matches[0][0] ) && isset( $matches[1][0] ) ) {
+		foreach ( $matches[0] as $key => $match ) {
+			if ( false !== strpos( $matches[0][ $key ], 's.w.org/images/core/emoji' ) && isset( $matches[1][ $key ] ) ) {
+				$activity_content = str_replace( $matches[0][ $key ], $matches[1][ $key ], $activity_content );
 			}
 		}
 	}
 
-	$bp_sanitized = bp_activity_filter_kses( $activity_content );
-
-	if ( ! $emojis ) {
-		return $bp_sanitized;
-	}
-
-	foreach ( $emojis as $emoji ) {
-		$bp_sanitized = str_replace( $emoji['bp_sanitized'], $emoji['bp_should_allow'], $bp_sanitized );
-	}
-
-	return $bp_sanitized;
+	return $activity_content;
 }
-
-// Disable too restrictive filters.
-remove_filter( 'bp_get_activity_content_body', 'bp_activity_filter_kses', 1 );
-remove_filter( 'bp_get_activity_parent_content', 'bp_activity_filter_kses', 1 );
-remove_filter( 'bp_get_activity_latest_update', 'bp_activity_filter_kses', 1 );
-remove_filter( 'bp_get_activity_latest_update_excerpt', 'bp_activity_filter_kses', 1 );
-remove_filter( 'bp_get_activity_feed_item_description', 'bp_activity_filter_kses', 1 );
-remove_filter( 'bp_activity_content_before_save', 'bp_activity_filter_kses', 1 );
-remove_filter( 'bp_activity_action_before_save', 'bp_activity_filter_kses', 1 );
-remove_filter( 'bp_activity_latest_update_content', 'bp_activity_filter_kses', 1 );
-
-// Replace the disabled filters by one suitable for activity blocks
-add_filter( 'bp_get_activity_content_body', __NAMESPACE__ . '\activity_blocks_kses', 1 );
-add_filter( 'bp_get_activity_parent_content', __NAMESPACE__ . '\activity_blocks_kses', 1 );
-add_filter( 'bp_get_activity_latest_update', __NAMESPACE__ . '\activity_blocks_kses', 1 );
-add_filter( 'bp_get_activity_latest_update_excerpt', __NAMESPACE__ . '\activity_blocks_kses', 1 );
-add_filter( 'bp_get_activity_feed_item_description', __NAMESPACE__ . '\activity_blocks_kses', 1 );
-add_filter( 'bp_activity_content_before_save', __NAMESPACE__ . '\activity_blocks_kses', 1 );
-add_filter( 'bp_activity_action_before_save', __NAMESPACE__ . '\activity_blocks_kses', 1 );
-add_filter( 'bp_activity_latest_update_content', __NAMESPACE__ . '\activity_blocks_kses', 1 );
+add_filter( 'bp_activity_content_before_save', __NAMESPACE__ . '\activity_blocks_preserve_emoji_chars', 2 );
 
 /**
  * Allow usage of the paragraph tag and the link’s target attribute into Activities content.
@@ -194,36 +126,3 @@ function activity_blocks_allowed_tags( $tags = array() ) {
 	return array_merge( $tags, array( 'p' => true ) );
 }
 add_filter( 'bp_activity_allowed_tags', __NAMESPACE__ . '\activity_blocks_allowed_tags' );
-
-/**
- * Moderate the posted activity item, if it contains moderate keywords.
- *
- * @since 1.0.0
- *
- * @param BP_Activity_Activity $activity The activity object to check.
- */
-function activity_check_moderation_keys( $activity ) {
-
-	// Only check specific types of activity updates.
-	if ( ! in_array( $activity->type, bp_activity_get_moderated_activity_types(), true ) ) {
-		return;
-	}
-
-	// Remove WP Emojis from the content to moderate.
-	$content_to_moderate = preg_replace( '/src=\"(https|http):\/\/s.w.org\/images\/core\/emoji.*?\"/', '', $activity->content );
-
-	// Send back the error so activity update fails.
-	$moderate = bp_core_check_for_moderation( $activity->user_id, '', $content_to_moderate, 'wp_error' );
-	if ( is_wp_error( $moderate ) ) {
-		$activity->errors = $moderate;
-
-		// Backpat.
-		$activity->component = false;
-	}
-}
-
-// Activity links moderation shouldn't take WP Emoji links in account.
-remove_action( 'bp_activity_before_save', 'bp_activity_check_moderation_keys', 2, 1 );
-
-// Use a more suitable moderation function for activity blocks.
-add_action( 'bp_activity_before_save', __NAMESPACE__ . '\activity_check_moderation_keys', 2, 1 );
