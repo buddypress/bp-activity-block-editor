3f7579c3b01bf55d1590696d703d1471ddb08fd5
imath <math.viet@gmail.com>
2024-09-30 19:30:41 +0200

    Update package json lock file

--- bp-activity/assets/stream/index.asset.php
+++ bp-activity/assets/stream/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-dom-ready', 'wp-i18n', 'wp-url'), 'version' => 'ae854947706396cac1bb');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-dom-ready', 'wp-i18n', 'wp-url'), 'version' => '78b364a30ed5ec7521cf');
